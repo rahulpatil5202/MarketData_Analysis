e4cf0ce40baf02b7d3e97ebd171ef14073b0fd06
rahulpatil5202 <rahul.patil5202@gmail.com>
2018-05-31 20:10:48 +0530

    office closed

--- Scripts/timeSeries_Practice.R
+++ Scripts/timeSeries_Practice.R
@@ -6,6 +6,7 @@ library(timeSeries)
 library(Quandl)
 library(forecast)
 library(zoo)
+library(xts)
 
 # quandl_api_key('CuryXaHBCuZX8LsRs1g1')
 # YES_BANK = Quandl("NSE/YESBANK",collapse="daily",start_date="2017-05-15",type="raw")
@@ -24,7 +25,19 @@ YES_BANK$Adj.Close <- as.numeric(as.character(YES_BANK$Adj.Close))
 # Now remove NAs by complete.cases
 YES_BANK <- YES_BANK[complete.cases(YES_BANK),] 
 
-yes_ts <- read.zoo(YES_BANK[,c("Date","Adj.Close")], header = T, format = "%Y-%m-%d")
+yes_xts <- as.xts(YES_BANK[,-1],order.by = YES_BANK$Date, dateFormat = "%Y-%m-%d")
+
+start(yes_ts)
+end(yes_ts)
+frequency(yes_ts)
+periodicity(yes_ts)
+
+
+plot.zoo(yes_ts, plot.type = "single")
+
+plot(diff(log(yes_ts)), plot.)
+
+acf(x = log(yes_ts), lag.max = 20, na.action = na.pass)
 
 train_ts <- yes_ts[1:3000]
 
@@ -37,3 +50,7 @@ act_vals<- yes_ts[3001:3010]
 
 pred_vals$pred
 YES_BANK[3001:3010,"Adj.Close"]
+
+head(yes_ts)
+head(log(yes_ts))
+head(diff(log(yes_ts)))
