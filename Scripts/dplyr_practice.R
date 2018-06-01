724bc7fa080312d4a39b09ac2b0ed88ffceeca66
rahulpatil5202 <rahul.patil5202@gmail.com>
2018-06-02 00:49:22 +0530

    1

--- Scripts/dplyr_practice.R
+++ Scripts/dplyr_practice.R
@@ -1,29 +1,35 @@
-install.packages("WDI")
-install.packages("dplyr")
-install.packages("reshape")
-
+# install.packages("WDI")
+# install.packages("dplyr")
+# install.packages("reshape")
+install.packages("tidyverse")
+library(tid)
 library(WDI)
 library(dplyr)
 library(reshape)
 
 #wdi_def <- read.csv("E:/datasets/WDI_Definition and Source.csv", header = T)
-dat <- read.csv("E:/datasets/WDI_Data.csv", header = T)
+dat <- read_csv("E:/datasets/WDI_Data.csv", header = T, check.names = T, stringsAsFactors = F) #Check.names for 
+#auto adjusting wrong variable names
 
 head(dat)
 str(dat)
 summary(dat)
 
+colnames(dat)
+dat <- dplyr::rename(dat, Series.Name = �..Series.Name) #Notice it changed column name to LHS of = and also
+
+
 #Unique Indicators series name
-unique(dat$ï..Series.Name)
+unique(dat$Series.Name)
 
 #filter single coulmn condition
 dat[dat$Country.Name == "Afghanistan", ]
 
 #filter mutiple column conditions 
-dat[dat$Country.Name == "India" & dat$ï..Series.Name == "Surface area (sq. km)",]
+dat[dat$Country.Name == "India" & dat$Series.Name == "Surface area (sq. km)",]
 
 #filter mutiple column conditions
-dat[(dat$Country.Name == "India" | dat$Country.Name == "Pakistan") & dat$ï..Series.Name == "Poverty headcount ratio at national poverty lines (% of population)",]
+dat[(dat$Country.Name == "India" | dat$Country.Name == "Pakistan") & (dat$Series.Name == "Poverty headcount ratio at national poverty lines (% of population)"),]
 
 #Sort on column value
 arrange(dat, dat$Country.Code)
@@ -35,24 +41,49 @@ tail(arrange(dat, dat$Country.Code, dat$Country.Name))
 
 select(dat, Country.Code, Country.Name) # output of two coulmns as selected
 
-colnames(dat)
-
-rename(dat, Series.Name = ï..Series.Name) #Notice it changed column name to LHS of = and also
 #returned all other columns
-rename(iris, petal_length = Petal.Length)
+#Note dplyr rename gets masked when you load reshape library
+
+dplyr::rename(iris, petal_length = Petal.Length)
 
 #Select distinct values
-distinct(select(dat, ï..Series.Name))
+distinct(select(dat, Series.Name))
 
-distinct(select(dat, Country.Code, ï..Series.Name))
+distinct(select(dat, Country.Code, Series.Name))
 
 head(dat)
+dim(dat)
 
 #Unpivoting Data
 
-melt_dat <- melt(dat, id.vars = c("Country.Code", "Country.Name", "Series.Code", "ï..Series.Name"))
+melt_dat <- melt(dat, id.vars = c("Country.Code", "Country.Name", "Series.Code", "Series.Name"))
 
 melt_dat #Notice that it melted other variables not menioned in melt command and created
 #two new columns for all other variable and their count.
 #fantastically reshaped in R
 
+#Use Group by and summarise for pivoting functions
+
+str(melt_dat)
+
+#change all classes to continuous variables and rename column
+
+melt_dat$Country.Code <-  as.character(melt_dat$Country.Code)
+melt_dat$Country.Name <-  as.character(melt_dat$Country.Name)
+melt_dat$Series.Code <- as.character(melt_dat$Series.Code)
+melt_dat$Series.Name <- as.character(melt_dat$Series.Name)
+
+melt_dat <- dplyr::rename(melt_dat, Yearx=variable)
+
+melt_dat$Yearx <- as.character(melt_dat$Yearx)
+melt_dat$value <- as.numeric(melt_dat$value)
+
+
+#group by and summarise melt_dat
+melt_dat %>% filter(!is.na(Series.Name) | Series.Name != " ")%>%
+  group_by(Series.Name, Yearx, value)%>%
+  summarise(count=n(), total=sum(value), avg=mean(value))
+
+head(melt_dat$Series.Name)
+
+summary(melt_dat$Series.Name)
