878d25fb4ba0a8bd5e21150e8d3174b046cef9b7
rahulpatil5202 <rahul.patil5202@gmail.com>
2018-06-05 11:38:08 +0530

    1138 Office

--- Scripts/WDI_data.R
+++ Scripts/WDI_data.R
@@ -8,4 +8,30 @@ wdi.data <- read.csv("E:/datasets/WDI_Data.csv",header = T, stringsAsFactors = F
 
 str(wdi.data)
 
+wdi.data <- rename(wdi.data, Series.Name = ï..Series.Name)
+wdi.data <- rename_at(wdi.data, vars(contains("YR")), function(x) str_extract_all(x, pattern = "YR[0-9]+", simplify = T))
+
+#Take off commas from first column
+
+wdi.data$Series.Name <- gsub(",","", wdi.data$Series.Name)
+
+str(wdi.data)
+dim(wdi.data)
+
+#out of 22 columns first four are character class, rest all are numeric
+
+for(i in 5:22) {
+  wdi.data[[i]] <- as.numeric(wdi.data[[i]])
+}
+
+str(wdi.data)
+
+wdi.data <- wdi.data[,-22]
+
+ind_pak <- wdi.data %>% 
+  filter(wdi.data$Country.Name == "India" | wdi.data$Country.Name == "Pakistan")
+  
+
+head(ind_pak)
+
 
