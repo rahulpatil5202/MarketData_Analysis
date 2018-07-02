20f852729f3f5d1a08c61dd1788dc8719cc66c3c
rahulpatil5202 <rahul.patil5202@gmail.com>
2018-07-02 18:03:56 +0530

    as

---
+++ Scripts/sbilife_analysis.R
@@ -0,0 +1,29 @@
+library(RPostgreSQL)
+library(RODBC)
+library(tidyverse)
+library(lubridate)
+library(corrplot)
+
+#connecting to postgreSQL
+
+cn1 <- dbConnect(PostgreSQL(), host = 'localhost', port = 5432,dbname = 'data_science', user = 'rahul', password='postgres@123')
+
+sbilife <- dbGetQuery(cn1, "select nse.trade_date, demat.exchange,demat.scrip_code, demat.isin, demat.dp_bal as current_dpBal, demat.hold_price as dp_holdprice, nse.close, nse.prevclose, 
+nse.tottrdqty,nse.tottrdval, nse_indices.index_name, nse_indices.closing_index_value from demat
+                      inner join nse on demat.isin = nse.isin
+                      left outer join nse_indices on nse.trade_date = nse_indices.index_date
+                      where nse_indices.index_name = 'Nifty 50' and nse.symbol = 'SBILIFE'
+                      order by nse.trade_date, demat.scrip_code")
+
+
+ggplot(sbilife, aes(x=trade_date, y=close))+
+  geom_line(stat = 'identity')+
+  geom_line(aes(y=sbilife$closing_index_value/15, color='red'))
+
+
+# connecting to MSSQLSERVER
+cn2 <- odbcConnect(dsn = 'localSQL', uid = 'rahul', pwd = 'sql@123') #localSQL dsn created in windows to connect to DB
+
+sqlQuery(channel = cn2, query = 'select * from dbo.demat')
+
+
