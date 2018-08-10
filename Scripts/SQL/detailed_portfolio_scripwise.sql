29dc378b5afe2fea98e87e9ed062e86c42311843
rahulpatil5202 <rahul.patil5202@gmail.com>
2018-08-10 11:48:41 +0530

    office

--- Scripts/SQL/detailed_portfolio_scripwise.sql
+++ Scripts/SQL/detailed_portfolio_scripwise.sql
@@ -1,6 +1,6 @@
 select nse.trade_date, nse.symbol, nse.close, sum(demat.hold_value)as investedvalue,(demat.dp_bal * nse.close) as currentvalue,
 ROUND(((demat.dp_bal * nse.close) - lag((demat.dp_bal * nse.close)) over(order by symbol, trade_date))::numeric,2) as days_gain,
-ROUND((((demat.dp_bal * nse.close) - lag((demat.dp_bal * nse.close)) over(order by symbol, trade_date))/sum(demat.hold_value)*100)::numeric,2) as days_gain_percent,
+ROUND((((demat.dp_bal * nse.close) - lag((demat.dp_bal * nse.close)) over(order by symbol, trade_date))/lag((demat.dp_bal * nse.close)) over(order by symbol, trade_date))::numeric,2) as days_gain_percent,
 ROUND(((demat.dp_bal * nse.close) - sum(demat.hold_value))::numeric,2) as overallgain,
 ROUND((((demat.dp_bal * nse.close) - sum(demat.hold_value))/sum(demat.hold_value)*100)::numeric,2) as overallgain_percent,
 nse_indices.closing_index_value as Nifty,
